2a3aa67792f088a0e1ee98c8b52e5cd6bb8c33cf
HlavatovicMarek <247115@vutbr.cz>
2024-11-22 13:37:49 +0100

    treti commit

--- main.py
+++ main.py
@@ -1,26 +1,35 @@
 import numpy as np
+from scipy.optimize import curve_fit
+
+def spiral_model(t, a, b):
+    return a * t * np.cos(t), a * t * np.sin(t)
 
 def calculate_trajectory_deviation(points):
     """
-    Vypočíta odchýlku trajektórie od priamky.
+    Vypočíta odchýlku trajektórie od špirály.
     points: Zoznam dvojíc reprezentujúcich (x, y) súradnice bodov v trajektórii.
     """
-    # Prispôsobí priamku bodom
     x_coords, y_coords = zip(*points)
-    coefficients = np.polyfit(x_coords, y_coords, 1)
-    polynomial = np.poly1d(coefficients)
-    
-    # Vypočíta odchýlku od prispôsobenej priamky
-    deviations = [abs(polynomial(x) - y) for x, y in points]
-    
+    t_coords = np.linspace(0, 4 * np.pi, len(points))  # Predpokladáme, že body tvoria špirálu
+
+    # Prispôsobí špirálový model bodom
+    def wrapped_spiral_model(t, a, b):
+        x, y = spiral_model(t, a, b)
+        return np.concatenate((x, y))
+
+    params, _ = curve_fit(wrapped_spiral_model, t_coords, np.concatenate((x_coords, y_coords)))
+    a, b = params
+
+    # Vypočíta odchýlku od prispôsobenej špirály
+    fitted_x, fitted_y = spiral_model(t_coords, a, b)
+    deviations = [np.sqrt((x - fx)**2 + (y - fy)**2) for (x, y), (fx, fy) in zip(points, zip(fitted_x, fitted_y))]
+
     return deviations
 
 def calculate_speed_changes(points, times):
-    """
-    Vypočíta zmeny rýchlosti na začiatku, v strede a na konci trajektórie.
-    points: Zoznam dvojíc reprezentujúcich (x, y) súradnice bodov v trajektórii.
-    times: Zoznam časových značiek zodpovedajúcich každému bodu v trajektórii.
-    """
+    #
+     #Vypočíta zmeny rýchlosti na začiatku, v strede a na konci trajektórie. points: Zoznam dvojíc reprezentujúcich (x, y) súradnice bodov v trajektórii. times: Zoznam časových značiek zodpovedajúcich každému bodu v trajektórii.
+
     speeds = []
     
     for i in range(1, len(points)):
@@ -29,11 +38,11 @@ def calculate_speed_changes(points, times):
         speed = distance / time_diff
         speeds.append(speed)
     
-    # Vypočíta zmeny rýchlosti na začiatku, v strede a na konci tuto musím konkrétne zadať ako chvem ú
+    # Vypočíta zmeny rýchlosti na začiatku, v strede a na konci
     speed_changes = {
-        "beginning": speeds[1] - speeds[0],
-        "middle": speeds[len(speeds)//2 + 1] - speeds[len(speeds)//2],
-        "end": speeds[-1] - speeds[-2]
+        "začiatok": speeds[1] - speeds[0],
+        "stred": speeds[len(speeds)//2 + 1] - speeds[len(speeds)//2],
+        "koniec": speeds[-1] - speeds[-2]
     }
     
     return speed_changes
